c7b1a7d3e2b8c429234913d5d2698b0b39d5e505
svetozar12 <zaauna@abv.bg>
2023-02-03 15:43:46 -0800

    .

--- webpack-ts/webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const { merge } = require("webpack-merge");
-const commonConfig = require("./webpack.config.js");
-
-const prodConfig = {
-  mode: "production",
-};
-
-module.exports = merge(commonConfig, prodConfig);
